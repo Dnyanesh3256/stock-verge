64e92233e6c8d84cbef9f312fa702aefde6d7aba
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-08-12 00:08:03 +0530

    created navbar of the webpage

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return (
-        <h1>Navbar</h1>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+      <div className="container p-2">
+        <a className="navbar-brand" href="#">
+          <img
+            src="media/images/logoStockVerge.svg"
+            alt="Logo"
+            style={{ width: "40%" }}
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          {/* This ul will be pushed to the right */}
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Signup
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Products
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
