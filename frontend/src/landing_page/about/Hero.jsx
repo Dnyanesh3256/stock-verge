f3e8db9a01b807ebb43288851480b690be7296f0
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-08-15 00:43:14 +0530

    designed the about page

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom"
 
 function Hero() {
-    return (
-        <h1>Hero</h1>
-    );
+  return (
+    <div className="container">
+      <div className="row text-center p-5">
+        <h2 className="fs-3 pt-5 text-muted">
+          We pioneered the discount broking model in India.
+          <br />
+          Now we are breaking ground with our technology.
+        </h2>
+      </div>
+
+      <div className="row mt-5 border-top p-5 text-muted justify-content-center" style={{fontSize: "1em", lineHeight: "1.8"}}>
+        <div className="col-6 p-5" style={{maxWidth: "500px"}}>
+          <p>
+            We kick-started operations on the 15th of August, 2010 with the goal
+            of breaking all barriers that traders and investors face in India in
+            terms of cost, support, and technology. We named the company
+            StockVerge, to signify our mission of bringing investors to the
+            verge of new opportunities.
+          </p>
+          <p>
+            Today, our disruptive pricing models and in-house technology have
+            made us the biggest stock broker in India.
+          </p>
+          <p>
+            Over 1.6+ crore clients place billions of orders every year through
+            our powerful ecosystem of investment platforms, contributing over
+            15% of all Indian retail trading volumes.
+          </p>
+        </div>
+        <div className="col-6 p-5" style={{maxWidth: "500px"}}>
+          <p>
+            In addition, we run a number of popular open online educational and
+            community initiatives to empower retail traders and investors.
+          </p>
+          <p>
+            <Link style={{textDecoration: "none"}}>Rainmatter</Link>, our fintech fund and incubator, has invested in several
+            fintech startups with the goal of growing the Indian capital
+            markets.
+          </p>
+          <p>
+            And yet, we are always up to something new every day. Catch up on
+            the latest updates on our blog or see what the media is saying about
+            us or learn more about our business and product philosophies.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- frontend/src/landing_page/about/Team.jsx
+++ frontend/src/landing_page/about/Team.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Team() {
-    return (
-        <h1>Team</h1>
-    );
+  return (
+    <div className="container">
+      <div className="row text-center border-top">
+        <h2 className="text-muted mt-5">People</h2>
+      </div>
+
+      <div
+        className="row p-3 text-muted"
+        style={{ fontSize: "1em", lineHeight: "1.8" }}
+      >
+        <div className="col-6 p-5 text-center">
+          <img
+            src="media/images/Passport_Pic_DT.jpg"
+            alt="DT Image"
+            style={{ width: "55%", borderRadius: "100%" }}
+          />
+          <h4 className="mt-2">Dnyaneshwar Tupe</h4>
+          <h6>Founder, CEO</h6>
+        </div>
+        <div className="col-6 p-5 mt-4" style={{fontSize: "17px"}}>
+          <p>
+            Dnyaneshwar architects and builds full-stack applications,
+            transforming complex challenges into clean, scalable solutions. His
+            expertise lies in the MERN stack, where he engineers both robust
+            backends and intuitive frontends.
+          </p>
+          <p>
+            Tackling new challenges and architecting user-centric solutions is
+            his zen.
+          </p>
+          <p>
+            Connect on <Link style={{ textDecoration: "none" }}>Portfolio</Link>{" "}
+            / <Link style={{ textDecoration: "none" }}>LinkedIn</Link> / <Link style={{ textDecoration: "none" }}>GitHub</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Team;
